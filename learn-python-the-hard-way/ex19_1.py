b8c6cb94e8775e54795d37ec8a520815c0a87d6d
26huitailang <26huitailang@gmail.com>
2015-12-17 18:20:51 +0800

    ex21

--- learn-python-the-hard-way/ex19_1.py
+++ learn-python-the-hard-way/ex19_1.py
@@ -1,1 +1,11 @@
 # Write at least one more function of your own design, and run it 10 different ways.
+def appending_txt(new_txt, filename):
+    indata = open(filename, 'a')
+    indata.write("\n")
+    indata.write(new_txt)
+    indata.close()
+
+file = raw_input("What's the filename?\n> ")
+user_input = raw_input("What do U want to appending?\n> ")
+
+appending_txt(user_input, file)
