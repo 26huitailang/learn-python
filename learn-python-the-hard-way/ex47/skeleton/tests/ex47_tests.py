5aedf8ff8f9e4aab287288b4bcf086a1949cb59d
26huitailang <26huitailang@gmail.com>
2016-03-02 17:08:35 +0800

    ex49 assert_raises

--- learn-python-the-hard-way/ex47/skeleton/tests/ex47_tests.py
+++ learn-python-the-hard-way/ex47/skeleton/tests/ex47_tests.py
@@ -1,31 +1,42 @@
 from nose.tools import *
 from ex47.game import *
 
+a_map = Map()
 def test_room():
-    startroom = StartRoom("StartRoom", "U are in Start Room. There's two ways: West or Down")
-    assert_equal(startroom.name, "StartRoom")
-    print "Room worked well!"
-    assert_equal(startroom.paths, {'west': 'trees', 'down': 'dungeon'})
-    print "Paths well: %s" % startroom.paths
+    # startroom = Room("StartRoom", "You can go west and down a hole.")
+    # trees = Room("Trees", "Trees room in the west.")
+    # dungeon = Room("Dungeon", "It's dark down here, you can go up.")
+    assert_equal(a_map.startroom.name, "StartRoom")
+    assert_equal(a_map.trees.name, "Trees")
+    # assert_equal(dungeon.name, "Dungeon")
+    # assert_equal(goldroom.name, "GoldRoom")
+    # print "Room worked well!"
+    assert_equal(a_map.startroom.paths, {'west': 'Trees', 'down': 'Dungeon'})
+    assert_equal(a_map.dungeon.paths,{'up': 'StartRoom'})
+    # trees.add_paths({'east': 'StartRoom', 'up': 'GoldRoom'})
+    # goldroom.add_paths({'north': 'TestRoom'})
+    # print "Paths well: %s" % startroom.paths
 
 def test_room_paths():
-    center = Room("Center", "Test room in the center.")
-    north = Room("North", "Test room in the north.")
-    south = Room("South", "Test room in the south.")
+    # startroom = Room("StartRoom", "You can go west and down a hole.")
+    # trees = Room("Trees", "Trees room in the west.")
+    # goldroom = Room("GoldRoom", "Gold room in the up.")
+    # south = Room("South", "Test room in the south.")
 
-    center.add_paths({'north': north, 'south': south})
-    assert_equal(center.go('north'), north)
-    assert_equal(center.go('south'), south)
+    # trees.add_paths({'up': goldroom, 'east': startroom})
+    assert_equal(a_map.trees.go('up'), 'GoldRoom')
+    # assert_equal(center.go('south'), south)
+    # print "Room Paths worked well!"
 
 def test_map():
-    start = Room("Start", "You can go west and down a hole.")
-    west = Room("Trees", "There are trees here, you can go east.")
-    down = Room("Dungeon", "It's dark down here, you can go up.")
+    # startroom = Room("StartRoom", "You can go west and down a hole.")
+    # trees = Room("Trees", "Trees room in the west.")
+    # dungeon = Room("Dungeon", "It's dark down here, you can go up.")
 
-    start.add_paths({'west': west, 'down': down})
-    west.add_paths({'east': start})
-    down.add_paths({'up': start})
+    # startroom.add_paths({'west': trees, 'down': dungeon})
+    # trees.add_paths({'east': startroom})
+    # dungeon.add_paths({'up': startroom})
 
-    assert_equal(start.go('west'), west)
-    assert_equal(start.go('west').go('east'), start)
-    assert_equal(start.go('down').go('up'), start)
+    assert_equal(a_map.next_room('GoldRoom'), a_map.goldroom)
+    # assert_equal(a_map.startroom.go('west').go('east'), a_map.startroom)
+    # assert_equal(a_map.startroom.go('down').go('up'), a_map.startroom)
