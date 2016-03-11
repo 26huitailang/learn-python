8dbcaf2aaba0b6eaea36e5d953850e3ece8b4571
26huitailang <26huitailang@gmail.com>
2016-03-11 18:23:37 +0800

    0311

--- learn-python-the-hard-way/ex52/gothonweb/bin/app_backup.py
+++ learn-python-the-hard-way/ex52/gothonweb/bin/app_backup.py
@@ -2,7 +2,7 @@ import web
 from gothonweb import map
 from random import randint
 
-web.config.debug = False
+# web.config.debug = False
 
 urls = (
   '/game', 'GameEngine',
@@ -14,7 +14,7 @@ app = web.application(urls, globals())
 # little hack so that debug mode works with sessions
 if web.config.get('_session') is None:
     store = web.session.DiskStore('sessions')
-    init = {'room': None}
+    init = {'room': None, 'guesses': 0}
     session = web.session.Session(app, store, initializer=init)
     web.config._session = session
 else:
@@ -37,38 +37,37 @@ class GameEngine(object):
             # elif session.room.name == "Laser Weapon Armory":
 
             else:
-                # num = session.get('guesses')
-                # print num, session.room.name
-                return render.show_room(room=session.room)
+                num = session.get('guesses')
+                print num, session.room.name
+                return render.show_room(room=session.room, nums=10-int(num))
         else:
             # why is there here? do you need it? catch the wrong input
             return render.you_died(quip=map.quips[randint(0, len(map.quips)-1)])
 
     def POST(self):
-        form = web.input(action=None)
+        form = web.input()
 
         # there is a bug here, can you fix it?
         if session.room and form.action:
-            session.room = session.room.go(form.action)
-            # try:
-            #     int(form.action)
-            #     if session.room.name == "Laser Weapon Armory":
-            #         # guesses = 0
-            #         while (form.action != map.code) and (session.guesses < 10):
-            #             print "BZZZZEDDD!"
-            #             print session.guesses
-            #             session.guesses += 1
-            #             # web.seeother("/game")
-            #             form = web.input()
-            #             # guess = raw_input("[keypad]> ")
-            #         if form.action == map.code:
-            #             session.room = session.room.go(form.action)
-            #         else:
-            #             session.room = None
-            #             web.seeother("/game")
-            # except ValueError:
-            #     session.room = session.room.go(form.action)
-        web.seeother("/game")
+            try:
+                int(form.action)
+                if session.room.name == "Laser Weapon Armory":
+                    # guesses = 0
+                    while (form.action != map.code) and (session.guesses < 10):
+                        print "BZZZZEDDD!"
+                        print session.guesses
+                        session.guesses += 1
+                        # web.seeother("/game")
+                        form = web.input()
+                        # guess = raw_input("[keypad]> ")
+                    if form.action == map.code:
+                        session.room = session.room.go(form.action)
+                    else:
+                        session.room = None
+                        web.seeother("/game")
+            except ValueError:
+                session.room = session.room.go(form.action)
+                web.seeother("/game")
 
 if __name__ == "__main__":
     app.run()
--- learn-python-the-hard-way/ex52/gothonweb/tests/app_tests.py
+++ learn-python-the-hard-way/ex52/gothonweb/tests/app_tests.py
@@ -1,35 +1,64 @@
 from nose.tools import *
-from bin.app import app
+from bin import app
 from tests.tools import assert_response
-from bin import app as aapp
+import web
+from gothonweb import map
 
-def setUp():
-    pass
+# def test_Index():
+#     # check that we get a 404 on the / URL
+#     resp = app.request("/aaa")
+#     assert_response(resp, status="404")
+#
+#     # test our first GET request to /hello
+#     resp = app.request("/")
+#     assert_response(resp)
+
+# @with_setup(setUp())
+class test_GameEngine():
+    if web.config.get('_session') is None:
+        store = web.session.DiskStore('sessions')
+        session = web.session.Session(app, store,
+                                      initializer={'room': None, 'guesses': 0})
+        web.config._session = session
+    else:
+        session = web.config._session
+
+    game = app.GameEngine()
+
+    def setUp(self):
+        self.session.room = map.START
+        self.session.guesses = 0
+        print "set up!"
+
+    def tearDown(self):
+        # print session.room.name
+        # session.room = map.START
+        print "tear down!"
 
 
-def test_Index():
-    # check that we get a 404 on the / URL
-    resp = app.request("/aaa")
-    assert_response(resp, status="404")
+    def test_GET(self):
+        # make sure default values work for the form
+        # resp = self.game.GET()
+        resp = app.app.request("/game", method="GET")
+        # print resp
+        assert_response(resp, contains=None)
 
-    # test our first GET request to /hello
-    resp = app.request("/")
-    assert_response(resp)
 
-@with_setup(setUp())
-def test_GameEngine():
-
-    # make sure default values work for the form
-    resp = app.request("/game", method="GET")
-    assert_response(resp, contains="Central Corridor")
-
-def test_POST():
-    # test that we get expected values
-    data = {'action': 'tell a joke'}
-    resp = app.request("/game", method="POST", data=data)
-    print resp
-    resp2 = aapp.session.get('room')
-    print resp2
-    # assert_equal(resp, laser_weapon_armory)
-    # resp = app.request("/game", method="GET")
-    # assert_response(resp, contains="Laser Weapon Armory")
+    def test_POST(self):
+        # test that we get expected values
+        data = {'action': 'tell a joke'}
+        resp1 = app.app.request("/")
+        # print self.session.room.name
+        resp2 = app.app.request("/game", method="GET")
+        assert_response(resp2, contains="Central Corridor")
+        resp3 = app.app.request("/game", method="POST", data=data)
+        resp = app.app.request("/game", method="GET")
+        print resp1
+        # resp = app.app.request("/", method="GET")
+        print resp2
+        print resp
+        # resp2 = aapp.session.get('room')
+        # print resp2
+        # assert_equal(resp, laser_weapon_armory)
+        # resp = app.request("/game", method="GET")
+        assert_response(resp, contains="Laser Weapon Armory")
