53798640ee18880ff6b1d4b45280b5f70faae2da
26huitailang <26huitailang@gmail.com>
2017-01-31 22:01:18 +0800

    modified(items.py): add Field()

--- spider/haoduofuli/haoduofuli/items.py
+++ spider/haoduofuli/haoduofuli/items.py
@@ -11,4 +11,9 @@ import scrapy
 class HaoduofuliItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    category = scrapy.Field()  # 类型
+    title = scrapy.Field()  # 标题
+    imgurl = scrapy.Field()  # 图片地址
+    yunlink = scrapy.Field()  # 云链接
+    password = scrapy.Field()  # 密码
+    url = scrapy.Field()  # 页面地址
