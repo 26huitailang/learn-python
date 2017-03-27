a718d942c7d3eba58d88b8d4f88140e156405953
26huitailang <26huitailang@gmail.com>
2017-03-27 17:17:42 +0800

    modified: 修改pipelines
    1. 使用新的对应dytt_zuixin的

--- spider/myscrapy/myscrapy/pipelines.py
+++ spider/myscrapy/myscrapy/pipelines.py
@@ -6,7 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
 from .items import DyttItem
-from .settings import DYTT_COL, DYTT_MONGODB
+from .settings import DYTT_COL, DYTT_MONGODB, DYTT_ZUIXIN_COL
 
 
 class MyscrapyPipeline(object):
@@ -30,6 +30,28 @@ class DyttPipeline(object):
                     '电影天堂标题': item['title_dytt'],
                     '海报大图': item['img_url'],
                     '电影截图': item['img_url_detail'],
+                    '下载链接': item['download_url'],
+                    'url': item['url']
+                }
+                self.col.save(post)
+                print('插入一条影片', '*' * 27)
+
+
+class DyttZuixinPipeline(object):
+    def __init__(self):
+        client = MongoClient()
+        db = client[DYTT_MONGODB]
+        self.col = db[DYTT_ZUIXIN_COL]
+
+    def process_item(self, item, spider):
+        if isinstance(item, DyttItem):
+            url = item['url']
+            if self.col.find_one({'url': url}):
+                print('该电影已经存在了', '-' * 27)
+            else:
+                post = {
+                    '电影天堂标题': item['title_dytt'],
+                    '海报大图': item['img_url'],
                     '下载链接': item['download_url'],
                     'url': item['url']
                 }
