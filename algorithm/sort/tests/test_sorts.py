22c3324d157eba6262fdfffefda481ae30aba2df
26huitailang <26huitailang@gmail.com>
2017-09-05 23:25:28 +0800

    modified: 更改文件名，basic_sort，新增两个算法
    1. 修改了文件名，新增两个算法，快速排序和希尔排序，并添加测试
    2. 为什么这样做，学习
    3. 影响markdown中文档路径的提示，已做更正

--- algorithm/sort/tests/test_sorts.py
+++ algorithm/sort/tests/test_sorts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
-from algorithm.sort.basicSort import *
+from algorithm.sort.basic_sort import *
 import pytest
 from random import randint
 import time
@@ -19,25 +19,39 @@ def run_timer(f):
     return function_timer
 
 
-@pytest.fixture(scope="module", params=[100, 1000, 10000])
+@pytest.fixture(scope="module", params=[100, 1000, 10000, 1000000])
 def create_list(request):
     nums = []
     for i in range(request.param):
         nums.append(randint(0, request.param))
     return nums
 
+#
+# @run_timer
+# def test_bubble_sort(create_list):
+#     origin_list = create_list.copy()
+#     sort_list = bubble_sort(origin_list)
+#     # print(sort_list, create_list, origin_list)
+#     assert sort_list != create_list
+#
+#
+# @run_timer
+# def test_insert_sort(create_list):
+#     origin_list = create_list.copy()
+#     sort_list = insert_sort(origin_list)
+#     # print(sort_list, create_list)
+#     assert sort_list != create_list
+
 
 @run_timer
-def test_bubble_sort(create_list):
+def test_shell_sort_two(create_list):
     origin_list = create_list.copy()
-    sort_list = bubble_sort(origin_list)
-    # print(sort_list, create_list, origin_list)
+    sort_list = shell_sort_two(origin_list)
     assert sort_list != create_list
 
 
 @run_timer
-def test_insert_sort(create_list):
+def test_quick_sort(create_list):
     origin_list = create_list.copy()
-    sort_list = insert_sort(origin_list)
-    # print(sort_list, create_list)
+    sort_list = quick_sort(origin_list, 0, len(origin_list) - 1)
     assert sort_list != create_list
