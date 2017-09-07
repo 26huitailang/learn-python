49c5725ea09bb62b6976691e6c28326d58660e5f
26huitailang <26huitailang@gmail.com>
2017-09-07 14:30:28 +0800

    add&modified: 排序算法
    1. 完成了8种基本算法
    2. 在tests中添加了图片绘制，将不同结果比较
    3. None

---
+++ algorithm/sort/plot_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+f = open('logs.json')
+data = json.load(f)
+print(data)
+
+lens = []
+funcs = []
+for key, value in data.items():
+    lens.append(int(key))
+    for k, v in value.items():
+        if k not in funcs:
+            funcs.append(k)
+
+# 重新分类数据
+y = {}
+for key in funcs:
+    y[key] = {}
+    for i in lens:
+        y[key][i] = data[str(i)][key]
+print(y)
+
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+plt.figure(figsize=(8, 4))
+plt.xlabel("Data length")
+plt.ylabel("Time(s)")
+plt.title("各个排序算法时间比较")
+for k, v in y.items():
+    x = lens
+    y = [value for key, value in v.items()]
+    plt.plot(x, y, label="%s" % k, marker='*')
+plt.legend()
+plt.show()
