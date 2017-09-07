a49963130fe0cb26a6417e8afaf805e1cd6a6be6
26huitailang <26huitailang@gmail.com>
2017-09-07 14:30:28 +0800

    add&modified: 排序算法
    1. 完成了8种基本算法
    2. 在tests中添加了图片绘制，将不同结果比较
    3. None

--- algorithm/sort/basic_sort.py
+++ algorithm/sort/basic_sort.py
@@ -5,6 +5,8 @@
 
 可以在/tests目录下进行测试对比，使用pytest。
 """
+import random
+import math
 
 
 def bubble_sort(arr):
@@ -13,23 +15,27 @@ def bubble_sort(arr):
     :param arr: 待排序列表
     :return: 排序后的列表
     """
+    # 每次循环留下 i 位置的最小值，值大的会向上冒泡
     for i in range(len(arr) - 1):
-        for j in range(len(arr) - 1 - i):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        # i 值往后的遍历
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
 
     return arr
 
 
 def insert_sort(arr):
-    """插入排序。
+    """ 插入排序。现实中的摸牌排序操作。
 
     :param arr: 待排序列表
     :return: 排序后的列表
     """
+    # 从两个数据开始，每次添加一位，直到覆盖整组数据
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
+        # 每次与前一位比较，直到新的数据插入完成
         while j >= 0:
             if arr[j] > key:
                 arr[j + 1] = arr[j]
@@ -40,7 +46,7 @@ def insert_sort(arr):
 
 
 def shell_sort(arr):
-    """希尔排序。
+    """ 希尔排序。
     
     :param arr: 待排序列表
     :return: 排序后的列表
@@ -55,51 +61,56 @@ def shell_sort(arr):
             temp = arr[j]
             while j >= step and arr[j] < arr[j - step]:
                 arr[j] = arr[j - step]
-                arr[j-step] = temp
+                arr[j - step] = temp
                 j -= step
-        step = step // 3
+        step //= 3
 
     return arr
 
 
 def shell_sort_two(arr):
-    """希尔排序，gap每次取一半，速度会比第一种方式快接近一倍。
+    """ 希尔排序，gap每次取一半，速度会比第一种方式快接近一倍。
 
     :param arr:
     :return:
     """
     count = len(arr)
-    step = 2
-    gap = count // step
+    step = 2  # 每次减小增量为上一个的一半
+    gap = count // step  # 增量
     while gap > 0:
         for i in range(0, gap):
-            j = i + gap
+            j = i + gap  # 离 i 位置gap距离的元素
             while j < count:
+                # 向前比较
                 k = j - gap
                 temp = arr[j]
+                # 如果前面的数大则交换位置，k继续向前，其实就是插入排序
                 while k >= 0 and arr[k] > arr[j]:
                     arr[j] = arr[k]
                     arr[k] = temp
                     k -= gap
+                # j往后探索，加入一个新的元素
                 j += gap
-        gap = gap // step
+        # 减小增量
+        gap //= step
 
     return arr
 
 
 def quick_sort(arr, low, high):
-    """快速排序，数据量大的时候比希尔快一倍左右。
+    """ 快速排序，数据量大的时候比希尔快一倍左右。
 
     :param arr: 待排序数据
     :param low: 低位
     :param high: 高位
     :return: 排序完成的数据
     """
-    i = low
-    j = high
+    i = low  # 最低位标志
+    j = high  # 最高位标志
+    # 探索位置重叠
     if i >= j:
         return arr
-    key = arr[i]
+    key = arr[i]  # 确定key值，大的放右边，小的放左边
     while i < j:
         while i < j and arr[j] >= key:
             j -= 1
@@ -107,31 +118,134 @@ def quick_sort(arr, low, high):
         while i < j and arr[i] <= key:
             i += 1
         arr[j] = arr[i]
+    # 完成一次比较后再将key值赋值
     arr[i] = key
+    # 继续将上次key值的结果递归
     quick_sort(arr, low, i - 1)
     quick_sort(arr, j + 1, high)
     return arr
 
 
-def select_sort():  # TODO
-    pass
+def select_sort(arr):
+    """ 直接选择排序。
+
+    :param arr: 待排序数据
+    :return: 已排序数据
+    """
+    count = len(arr)
+    for i in range(0, count):
+        low = i  # 记录第一个为最小值
+        for j in range(i + 1, count):
+            if arr[low] > arr[j]:
+                low = j  # 每次只记录比较最小值的位置
+        arr[low], arr[i] = arr[i], arr[low]  # 最小值放到当前列表的最前面
+
+    return arr
 
 
-def heap_sort():  # TODO
-    pass
+def max_heapify(arr, root, size):
+    """ 维护堆，使得子节点小于父节点。
+
+    :param arr: 待排序列表
+    :param root: 根节点
+    :param size: 列表大小
+    :return: None
+    """
+    lchild = 2 * root + 1
+    rchild = 2 * root + 2
+    largest = root
+    if root < size // 2:
+        if lchild < size and arr[lchild] > arr[largest]:
+            largest = lchild
+        if rchild < size and arr[rchild] > arr[largest]:
+            largest = rchild
+        if largest != root:
+            arr[largest], arr[root] = arr[root], arr[largest]
+            max_heapify(arr, largest, size)
 
 
-def merge_sort():  # TODO
-    pass
+def build_max_heap(arr, size):
+    """ 构建最大堆。
+
+    :param arr: 待排序数据
+    :return: None
+    """
+    for i in range(size // 2 - 1, -1, -1):
+        max_heapify(arr, i, size)
 
 
-def radix_sort():  # TODO
-    # TODO 完成所有算法后，可图形化展示不同量级的速度比较，matplotlib画图
-    pass
+def heap_sort(arr):
+    """ 堆排序。
+
+    :param arr: 待排序数据
+    :return: None
+    """
+    size = len(arr)
+    build_max_heap(arr, size)
+    for i in range(size - 1, -1, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        max_heapify(arr, 0, i)
+
+
+def merge(left, right):
+    """ 合并。
+
+    :param left: 左半序列
+    :param right: 右半序列
+    :return: 合并排序结果
+    """
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    # 当一边取完的时候，合并剩下的部分
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
+def merge_sort(arr):
+    """ 归并排序。
+
+    :param arr: 待排序数据
+    :return: 合并结果
+    """
+    if len(arr) <= 1:
+        return arr
+    q = len(arr) // 2
+    # 递归分解
+    left = merge_sort(arr[:q])
+    right = merge_sort(arr[q:])
+    return merge(left, right)
+
+
+def radix_sort(arr, radix=10):
+    """ 基数排序。属于分配式排序distribution sort，又称桶子法bucket sort。
+
+    :param arr: 待排序数据
+    :param radix: 桶的数量，默认10个
+    :return: 已排序数据
+    """
+    # 确定最大数的位数，决定排序趟数，ceil返回天花板值的float型
+    k = int(math.ceil(math.log(max(arr), 10)))
+    for i in range(k):
+        # 生成radix个桶，每次位循环完成清空buckets
+        buckets = [[] for i in range(radix)]
+        for j in arr:
+            buckets[j // radix ** i % radix].append(j)
+        arr = [num for bucket in buckets for num in bucket]
+
+    return arr
 
 
 if __name__ == '__main__':
-    arr = [12, 123, 34, 45, 767, 2, 5, 7, 9, 123, 34345, 234134234]
+    arr = [i for i in range(20)]
+    random.shuffle(arr)
     arr_b = arr.copy()
-    res = quick_sort(arr_b, 0, len(arr_b) - 1)
+    res = radix_sort(arr_b)
     print(res)
--- algorithm/sort/tests/test_sorts.py
+++ algorithm/sort/tests/test_sorts.py
@@ -5,6 +5,11 @@ import pytest
 from random import randint
 import time
 from functools import wraps
+import json
+from copy import deepcopy
+
+func_time = {}
+res = {}
 
 
 def run_timer(f):
@@ -14,44 +19,84 @@ def run_timer(f):
         result = f(*args, **kwargs)
         te = time.time()
         print("\nTotal time running {0}: {1} seconds".format(f.__name__, str(te - ts)))
+        func_time[f.__name__] = te - ts
         return result
 
     return function_timer
 
 
-@pytest.fixture(scope="module", params=[100, 1000, 10000, 1000000])
+@pytest.fixture(scope="module", params=[100, 1000, 10000, 100000, 1000000])
 def create_list(request):
+    print("start")
     nums = []
     for i in range(request.param):
         nums.append(randint(0, request.param))
-    return nums
+    yield nums
+    print("end")
+    # print(func_time)
+    # 用copy赋值，千万不用直接赋值，对func_time的修改会影响前面的结果
+    res[request.param] = func_time.copy()
+    # print(res)
+    jsonObj = json.dumps(res)
+
+    f = open('./logs.json', 'w')
+    f.write(jsonObj)
+    f.close()
+
 
 #
 # @run_timer
 # def test_bubble_sort(create_list):
 #     origin_list = create_list.copy()
-#     sort_list = bubble_sort(origin_list)
-#     # print(sort_list, create_list, origin_list)
-#     assert sort_list != create_list
+#     sorted_list = bubble_sort(origin_list)
+#     # print(sorted_list, create_list, origin_list)
+#     assert sorted_list != create_list
 #
 #
 # @run_timer
 # def test_insert_sort(create_list):
 #     origin_list = create_list.copy()
-#     sort_list = insert_sort(origin_list)
-#     # print(sort_list, create_list)
-#     assert sort_list != create_list
+#     sorted_list = insert_sort(origin_list)
+#     # print(sorted_list, create_list)
+#     assert sorted_list != create_list
+#
+# @run_timer
+# def test_select_sort(create_list):
+#     origin_list = create_list.copy()
+#     sorted_list = select_sort(origin_list)
+#     assert sorted_list != create_list
 
 
 @run_timer
 def test_shell_sort_two(create_list):
     origin_list = create_list.copy()
-    sort_list = shell_sort_two(origin_list)
-    assert sort_list != create_list
+    sorted_list = shell_sort_two(origin_list)
+    assert sorted_list != create_list
 
 
 @run_timer
 def test_quick_sort(create_list):
     origin_list = create_list.copy()
-    sort_list = quick_sort(origin_list, 0, len(origin_list) - 1)
-    assert sort_list != create_list
+    sorted_list = quick_sort(origin_list, 0, len(origin_list) - 1)
+    assert sorted_list != create_list
+
+
+@run_timer
+def test_heap_sort(create_list):
+    origin_list = create_list.copy()
+    heap_sort(origin_list)
+    assert origin_list != create_list
+
+
+@run_timer
+def test_merge_sort(create_list):
+    origin_list = create_list.copy()
+    sorted_list = merge_sort(origin_list)
+    assert sorted_list != create_list
+
+
+@run_timer
+def test_radix_sort(create_list):
+    origin_list = create_list.copy()
+    sorted_list = radix_sort(origin_list)
+    assert sorted_list != create_list
