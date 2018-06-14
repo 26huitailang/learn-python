e33a929e7057650e68ccb4c53166dbf00b56975f
陈思剑 <chensijian@xiaoyin.local>
2018-06-15 00:00:47 +0800

    leetcode 167 finish

--- algorithm/LeetCodeOJ/167-Two-Sum-II.py
+++ algorithm/LeetCodeOJ/167-Two-Sum-II.py
@@ -5,9 +5,16 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(numbers)):
-            if (target - numbers[i]) in numbers[i:]:
+        # 用字典来操作
+        val_index_dict = dict()
+        for i, v in enumerate(numbers):
+            val_index_dict[v] = i
+        
+        for i in numbers:
+            if (target - i) in val_index_dict:
                 # 避免两个重复的值时只能定位到第一个
                 # 从切片中定位再加切片的位置
-                return [i + 1, numbers[i:].index(target - numbers[i]) + 1 + i]
-
+                pos1 = numbers.index(i) 
+                pos2 = val_index_dict[target - i]
+                if pos1 < pos2:
+                    return [pos1 + 1, pos2 + 1]
