872f3e1522005ab3d234538a72fccd1a2cbbcc08
26huitailang <26huitailang@gmail.com>
2018-09-28 10:33:34 +0800

    leetcode: 38

--- algorithm/LeetCodeOJ/38-Count-and-Say.py
+++ algorithm/LeetCodeOJ/38-Count-and-Say.py
@@ -12,10 +12,21 @@ class Solution:
         :type n: int
         :rtype: str
         """
-        if n == 1:
-            return n
-
-        while n - 1 > 0:
-            counter = 0
-
+        result = '1' 
+        while n - 1 > 0:  # loop for next word
+            # init
+            count = 0  # current count
+            temp = ''  # next say
+            char = result[0]  # counting character
+            for c in result:  # loop for current word
+                if c == char:  # same, plus 1
+                    count += 1
+                else:  # not same, update the say string
+                    temp += str(count) + char
+                    char = c  # change counting char
+                    count = 1
+            temp += str(count) + char
+            result = temp  # next word
+            n -= 1
+        return result
 
