7bc5a21fbfd80a7118d19566c099b82d1e45156c
26huitailang <26huitailang@gmail.com>
2020-02-29 11:59:04 +0800

    mv leetcode to LeetCode Project

--- algorithm/LeetCodeOJ/680-Valid-Palindrome-II.py
+++
@@ -1,7 +0,0 @@
-def solution(s):
-    l = len(s)
-    for i in range(len(s) // 2):
-        if s[i] != s[-1-i]:
-            # 左边或右边拿掉一个字符成立
-            return s[i: l-i-1] == s[i: l-i-1][::-1] or s[i+1: l-i] == s[i+1: l-i][::-1]
-    return True
--- algorithm/LeetCodeOJ/9-Palindrome-Number.py
+++
@@ -1,9 +0,0 @@
-def solution(x):
-    try:
-        r = str(x)[::-1]  # 翻转字符串
-        if int(r) == x:  # 取整型
-            return True
-        else:
-            return False
-    except ValueError as e:  # 如果负数会到这儿
-        return False
